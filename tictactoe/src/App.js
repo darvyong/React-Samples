4d6fdfac5f70dc8fdf55445ace3b4e4545162b12
Darvy Ong <darvyong@gmail.com>
2023-04-26 19:35:47 +1000

    updated history tictactoe

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -25,9 +25,7 @@ function calculateWinner(squares) {
     return null;
 }
 
-function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
@@ -46,8 +44,7 @@ function Board() {
         } else {
             nextSquares[index] = "O";
         }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     return (
@@ -72,6 +69,48 @@ function Board() {
     );
 }
 
+function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="App">
@@ -82,7 +121,7 @@ function App() {
             </p>
         </header>
         <body>
-            <Board />
+            <Game />
         </body>
         </div>
     );
